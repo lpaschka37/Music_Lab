d9e50bdc12802d729047e30d9e306eb5249834b0
Luke Paschka <lpaschka37@gmail.com>
2021-04-26 09:47:36 -0500

    updates to the connection file

--- connection/deets.js
+++ connection/deets.js
@@ -3,20 +3,21 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+// create connection to our db
+if( process.env.NODE_ENV === "production" && process.env.JAWSDB_URL ){
+  sequelize = new Sequelize(process.env.JAWSDB_URL)
 } else {
   sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
+    process.env.DB_NAME, 
+    process.env.DB_USER, 
+    process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
       port: 3306
     }
-  );
+  )
 }
+
 
 
 module.exports = sequelize;
